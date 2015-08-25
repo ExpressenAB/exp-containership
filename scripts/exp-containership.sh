796afa434effc82ec125894029ad105f04695e87
Mattias Nordberg <mattias.nordberg@gmail.com>
2015-08-25 13:24:45 +0200

    create default Dockerfile and docker-compose.yml if missing

--- scripts/exp-containership.sh
+++ scripts/exp-containership.sh
@@ -16,7 +16,7 @@ machine_name="exp-docker"
 _DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 _REV=$(git rev-parse --short HEAD)
 eval "$($_DIR/docopts.py -h "$help" -V "$version" : "$@")"
-init=0
+init=1
 reset=0
 build=0
 push=0
@@ -31,6 +31,7 @@ for arg in "${argv[@]}"; do
         init=1
         break
     elif [ "$arg" == "reset" ]; then
+        init=0
         reset=1
         break
     elif [ "$arg" == "build" ]; then
@@ -68,25 +69,6 @@ for arg in "${argv[@]}"; do
     fi
 done
 
-# setup boot2docker
-function boot2docker_shellinit {
-    if (which docker-machine >/dev/null); then
-        ls=$(docker-machine ls | grep "${machine_name}")
-        if [ -z "${ls}" ]; then
-            echo "Creating docker machine..."
-            docker-machine create --driver virtualbox "${machine_name}"
-        elif !(echo "${ls}" | grep "Running"); then
-            echo "Starting docker machine..."
-            docker-machine start "${machine_name}"
-        fi
-        eval $(docker-machine env "${machine_name}")
-    elif [ "${kernel}" != "Linux" ]; then
-      echo "Need Docker Machine to proceed, please install Docker Toolbox and run this script again"
-      open "https://www.docker.com/toolbox"
-      exit 1
-    fi
-}
-
 # reset
 if [ $reset == 1 ]; then
     if [ "${kernel}" != "Linux" ]; then
@@ -94,7 +76,7 @@ if [ $reset == 1 ]; then
             ls=$(docker-machine ls | grep "${machine_name}")
             if [ -n "${ls}" ]; then
                 echo "Removing docker machine..."
-                docker-machine rm "${machine_name}"
+                docker-machine rm "${machine_name}" >/dev/null
             fi
         fi
     fi
@@ -102,7 +84,29 @@ fi
 
 # init
 if [ $init == 1 ]; then
-  boot2docker_shellinit
+  if (which docker-machine >/dev/null); then
+      ls=$(docker-machine ls | grep "${machine_name}")
+      if [ -z "${ls}" ]; then
+          echo "Creating docker machine..."
+          docker-machine create --driver virtualbox "${machine_name}" >/dev/null
+      elif !(echo "${ls}" | grep "Running" >/dev/null); then
+          echo "Starting docker machine..."
+          docker-machine start "${machine_name}" >/dev/null
+      fi
+      eval $(docker-machine env "${machine_name}")
+  elif [ "${kernel}" != "Linux" ]; then
+    echo "Need Docker Machine to proceed, please install Docker Toolbox and run this script again"
+    open "https://www.docker.com/toolbox"
+    exit 1
+  fi
+
+  if ! [ -f "./Dockerfile" ]; then
+    cp "${_DIR}/../exp-containership/templates/Dockerfile" .
+  fi
+
+  if ! [ -f "./docker-compose.yml" ]; then
+    cp "${_DIR}/../exp-containership/templates/docker-compose.yml" .
+  fi
 fi
 
 # prebuild
@@ -118,7 +122,6 @@ fi
 
 # build
 if [ $build == 1 ]; then
-    boot2docker_shellinit
     echo "Building container $npm_package_name:$_REV"
     #
     if [ ! -e .dockerignore ]; then
@@ -131,14 +134,12 @@ EOF
 fi
 
 if [ $push == 1 ]; then
-    boot2docker_shellinit
     echo "Tagging and pushing $npm_package_name:$_REV container"
     docker tag -f $npm_package_name:$_REV $npm_package_config_exp_containership_repo/$npm_package_name:$_REV
     docker push $npm_package_config_exp_containership_repo/$npm_package_name:$_REV
 fi
 
 if [ $run == 1 ]; then
-    boot2docker_shellinit
     if !(which docker-compose >/dev/null); then
       echo "Need Docker Compose to proceed, please install Docker Toolbox and run this script again"
       open "https://www.docker.com/toolbox"
